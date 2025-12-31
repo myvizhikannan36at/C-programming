41b2d4ec696163f388968188a69b27b6dced7303
myvizhikannan36at <myk.offical.07@gmail.com>
2026-01-01 01:46:23 +0530

    Add files via upload

---
+++ program 42.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a = 5, b = 6;
+    int sum = a + b;
+    sum++;
+    printf("%d", sum);
+    return 0;
+}
---
+++ program 43.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a = 6, b = 2;
+    int diff = a - b;
+    diff--;
+    printf("%d", diff);
+    return 0;
+}
