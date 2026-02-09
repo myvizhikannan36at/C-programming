5303bba8aa0ba065c2d9c8abf58bc1f1db4b2af0
Myvizhi kannan A <myk.offical.07@gmail.com>
2026-02-09 15:20:20 +0530

    Add files via upload

---
+++ C Basic/print double.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    double point;
+    scanf("%lf",&point);
+    printf("%lf",point);
+    
+    return 0;
+}
---
+++ C Basic/print float.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float point;
+    scanf("%f",&point);
+    printf("%.2f",point);
+    
+    return 0;
+}
